7c536518ae940223ffbadc2195b75ce4abecdb4c
Liliya (Tacarlen) <l.feodotova296@yandex.ru>
2023-08-06 21:01:46 +0300

    first commit

---
+++ Bomb/BombApp.swift
@@ -0,0 +1,17 @@
+//
+//  BombApp.swift
+//  Bomb
+//
+//  Created by Лилия Феодотова on 06.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct BombApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
