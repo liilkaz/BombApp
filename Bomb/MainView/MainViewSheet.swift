6695a2a723b8ecae455188451b71664a285e64a1
dsm5e <justry24@gmail.com>
2023-08-07 18:09:47 +0300

    Add
    Добавил ассеты, мейн вью

---
+++ Bomb/MainView/MainViewSheet.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewSheet.swift
+//  Bomb
+//
+//  Created by dsm 5e on 07.08.2023.
+//
+
+import SwiftUI
+
+struct MainViewSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainViewSheet()
+}
