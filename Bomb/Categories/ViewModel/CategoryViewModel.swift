0aae73ba3bee9ce6c27bb897b54ba3ce89f0c5cc
KashMihdi <127855730+KashMihdi@users.noreply.github.com>
2023-08-12 21:27:26 +0300

    добавил json с вопросами по категориям

--- Bomb/Categories/ViewModel/CategoryViewModel.swift
+++ Bomb/Categories/ViewModel/CategoryViewModel.swift
@@ -15,12 +15,10 @@ final class CategoryViewModel: ObservableObject {
     
     init() {
         categories = CategoryData.shared.questions
-        getQuestions()
     }
     
     func save() -> [CategoryName] {
-        getQuestions()
-        return categories.filter(\.isSelected).map(\.name)
+         categories.filter(\.isSelected).map(\.name)
     }
     
     func getCategories(names: [CategoryName]) {
@@ -29,15 +27,5 @@ final class CategoryViewModel: ObservableObject {
             chosen.isSelected = names.contains(category.name) ? true : false
             return chosen
         }
-    }
-    
-    func getQuestions() {
-        if categories.filter(\.isSelected).flatMap(\.questions).isEmpty {
-            if let questions = categories.randomElement()?.questions {
-                self.questions = questions
-                return
-            }
-        }
-        questions = categories.filter(\.isSelected).flatMap(\.questions)
     }
 }
