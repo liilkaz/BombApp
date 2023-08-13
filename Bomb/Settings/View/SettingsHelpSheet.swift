37c396bb88f0c01a478699e52a4c65b1dbe96f6a
KashMihdi <127855730+KashMihdi@users.noreply.github.com>
2023-08-13 09:35:44 +0300

    изменил окно помощи экрана настроек

--- Bomb/Settings/View/SettingsHelpSheet.swift
+++ Bomb/Settings/View/SettingsHelpSheet.swift
@@ -31,54 +31,112 @@ struct SettingsHelpSheet: View {
                             .multilineTextAlignment(.center)
                         
                         VStack(alignment: .leading, spacing: 10) {
-                            Text("• Бомба взорвется в течении 10 секунд.")
-                                .foregroundStyle(Color.primaryTextColor)
-                                .lineLimit(2)
-                                .modifiedText(size: smallText)
+                            HStack(spacing: 20) {
+                                Text("Короткое")
+                                    .foregroundColor(.secondaryTextColor)
+                                    .modifiedText(size: 14)
+                                    .padding(.all, 10)
+                                    .frame(width: 130)
+                                    .background(Color.primaryTextColor)
+                                    .cornerRadius(15)
+                                
+                                Text("• Бомба взорвется в течение 10 секунд.")
+                                    .foregroundStyle(Color.primaryTextColor)
+                                    .lineLimit(2)
+                                    .modifiedText(size: smallText)
+                            }
                             
-                            Text("• Бомба взорвется в течении 20 секунд.")
-                                .foregroundStyle(Color.primaryTextColor)
-                                .lineLimit(2)
-                                .modifiedText(size: smallText)
+                            HStack(spacing: 20) {
+                                Text("Среднее")
+                                    .foregroundColor(.primaryTextColor)
+                                    .modifiedText(size: 14)
+                                    .padding(.all, 10)
+                                    .frame(width: 130)
+                                    .background(Color.mainBackground)
+                                    .cornerRadius(15)
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 15)
+                                            .stroke(
+                                                Color.primaryTextColor,
+                                                lineWidth: 3
+                                            )
+                                    )
+                                
+                                Text("• Бомба взорвется в течение 20 секунд.")
+                                    .foregroundStyle(Color.primaryTextColor)
+                                    .lineLimit(2)
+                                    .modifiedText(size: smallText)
+                            }
                             
-                            Text("• Бомба взорвется в течении 45 секунд.")
-                                .foregroundStyle(Color.primaryTextColor)
-                                .lineLimit(2)
+                            HStack(spacing: 20) {
+                                Text("Длинное")
+                                    .foregroundColor(.secondaryTextColor)
+                                    .modifiedText(size: 14)
+                                    .padding(.all, 10)
+                                    .frame(width: 130)
+                                    .background(Color.primaryTextColor)
+                                    .cornerRadius(15)
+                                
+                                Text("• Бомба взорвется в течение 45 секунд.")
+                                    .foregroundStyle(Color.primaryTextColor)
+                                    .lineLimit(2)
                                 .modifiedText(size: smallText)
+                            }
                             
-                            Text("• Бомба взорвется в течении 10-45 секунд.")
-                                .foregroundStyle(Color.primaryTextColor)
-                                .lineLimit(1)
+                            HStack(spacing: 20) {
+                                Text("Случайное")
+                                    .foregroundColor(.secondaryTextColor)
+                                    .modifiedText(size: 14)
+                                    .padding(.all, 10)
+                                    .frame(width: 130)
+                                    .background(Color.primaryTextColor)
+                                    .cornerRadius(15)
+                                
+                                Text("• Бомба взорвется в течение 10-45 секунд.")
+                                    .foregroundStyle(Color.primaryTextColor)
+                                    .lineLimit(2)
                                 .modifiedText(size: smallText)
-                            
-                            Text("• Если выбран режим С заданиями, то после взрыва бомбы на экране будет появляться задание для проигравшего игрока.")
-                                .foregroundStyle(Color.primaryTextColor)
-                                .lineLimit(4)
-                                .modifiedText(size: smallText)
+                            }
                         }
+                        .padding(.bottom)
                         
-                        Text("Так же в настройках \n можно")
+                        Text("•Если выбран режим «С Заданиями!», то после взрыва бомбы на экране будет появляться задание для проигравшего игрока.")
+                            .foregroundStyle(Color.primaryTextColor)
+                            .lineLimit(4)
+                            .modifiedText(size: smallText)
+                        
+                        Text("Также в настройках \n можно")
                             .foregroundStyle(Color.primaryTextColor)
                             .lineLimit(2)
                             .modifiedText(size: largeText)
                             .multilineTextAlignment(.center)
+                            .overlay(
+                                Rectangle()
+                                    .fill(Color.mainBackground)
+                                    .frame(height: 2),
+                                alignment: .center
+                            )
+                            .overlay(
+                                Rectangle()
+                                    .fill(Color.mainBackground)
+                                    .frame(width: 80, height: 2),
+                                alignment: .bottom
+                            )
 
                         VStack(alignment: .leading, spacing: 10) {
-                            Text("• Включить / Отключить фоновую музыку.")
+                            Text("• Включить / Отключить фоновую\nмузыку.")
                                 .foregroundStyle(Color.primaryTextColor)
                                 .lineLimit(2)
                                 .modifiedText(size: smallText)
                             
-                            Text("• Выбрать звуки для фоновой музыки, тиканья бомбы и взрыва.")
+                            Text("• Выбрать звуки для фоновой музыки,\nтиканья бомбы и взрыва.")
                                 .foregroundStyle(Color.primaryTextColor)
                                 .lineLimit(2)
                                 .modifiedText(size: smallText)
                         }
                     }
+                    .padding(.bottom, 100)
                 }
-                .padding(.bottom, bottomPadding)
-
-                Spacer()
             }
             .padding()
         }
